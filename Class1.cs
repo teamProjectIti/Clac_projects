ead5d9e3efd94eb82feac1899ac8c817c9088bbf
martina2@gmal.com <martina.17295791@compit.aun.edu.eg>
2022-01-31 14:54:40 +0200

    add class subs

---
+++ Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clac_project
+{
+    internal class subb
+    {
+        public int sub(int x, int y)
+        {
+            return x - y;
+        }
+    }
+}
