a047a0dd39157bee3d06a05c286ae31a18347312
unknown <beshoyamen5@gmail.com>
2022-01-31 13:51:48 +0200

    add project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace clac_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ sub.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clac_project
+{
+    internal class sub
+    {
+        public int num1 { get; set; }
+    }
+}
