36d315ec99c1171af0685c657fc4e3e6bbc3d4bd
gamalsalwa20@gmail.com <gamalsalwa20@gmail.com>
2022-01-31 14:45:45 +0200

    div

---
+++ div.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clac_project
+{
+    internal class div
+    {
+        public int division(int x,int y)
+        {
+            return x / y;
+        }
+    }
+}
